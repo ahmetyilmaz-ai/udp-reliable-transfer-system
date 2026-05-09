d45a8c8b9bd3404133b0745ffcaec5dbebb742ee
ahmetyilmazzz <ahyil.fb@gmail.com>
2026-05-09 10:52:51 +0300

    client-server temel iletişimi sağla

--- istemci.py
+++ istemci.py
@@ -1,32 +1,28 @@
-import json
 import socket
-import os
 
-CHUNK_SIZE = 1024
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 5005
-TIMEOUT = 1.0
-MAX_RETRIES = 5
-def split_file(file_path, chunk_size=CHUNK_SIZE):
-    chunks = []
-    with open(file_path, "rb") as file:
-        while True:
-            data = file.read(chunk_size)
-            if not data:
-                break
-            chunks.append(data)
-    return chunks
+BUFFER_SIZE = 1024
 
 def main():
-    file_path = input("gönderilecek dosya yolu")
-    if not os.path.exists(file_path):
-        print("dosya bulunamadı")
-        return
-    chunks = split_file(file_path)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    print("dosya okundu", file_path)
-    print("toplam dosya sayısı", len(chunks))
-    print("parça boyutu", CHUNK_SIZE)
+    message = "Merhaba sunucu"
+
+    client_socket.sendto(
+        message.encode("utf-8"),
+        (SERVER_IP, SERVER_PORT)
+    )
+
+    print("Mesaj sunucuya gönderildi")
+
+    data, server_address = client_socket.recvfrom(BUFFER_SIZE)
+
+    response = data.decode("utf-8")
+
+    print(f"Sunucudan cevap geldi: {response}")
+
+    client_socket.close()
 
 if __name__ == "__main__":
     main()
--- sunucu.py
+++ sunucu.py
@@ -1,1 +1,27 @@
 import socket
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 5005
+BUFFER_SIZE = 1024
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # AF_INET: IPv4, SOCK_DGRAM: UDP kullanılacağı anlamına gelir.
+
+    server_socket.bind((SERVER_IP, SERVER_PORT))
+
+    print(f"Sunucu başlatıldı: {SERVER_IP}:{SERVER_PORT}")
+    print("Mesaj bekleniyor...")
+
+    while True:
+        data, client_address = server_socket.recvfrom(BUFFER_SIZE)
+
+        message = data.decode("utf-8")
+
+        print(f"İstemciden mesaj geldi: {message}")
+        print(f"İstemci adresi: {client_address}")
+
+        response = "Mesaj alındı"
+        server_socket.sendto(response.encode("utf-8"), client_address)
+
+if __name__ == "__main__":
+    main()
